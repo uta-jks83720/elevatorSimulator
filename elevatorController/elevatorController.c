3c5a2ae9760370f750204115772ab621ddf08dfe
Bud Davis <jimmie.davis@uta.edu>
2024-12-29 17:26:00 -0600

    More functionality in the controller.

--- elevatorController/elevatorController.c
+++ elevatorController/elevatorController.c
@@ -1,5 +1,5 @@
 #include <assert.h>
-
+#include <stdio.h>
 
 #include "elevatorController.h"
 
@@ -16,10 +16,11 @@
 //
 //
 // Specifics of this sample controller:
-//       1.  On power on, the elevator goes to the bottom floor (floor 2), and opens the
-//           doors.
+//       1.  On power on, the elevator is initialized to the bottom floor (floor 2),
+//           doors open.
 //       2.  When a 'REQUEST' button is pressed, the elevator goes to that floor.
-//       3.  When a 'CALL' button is pressed, the elevator goes to that floor.
+//       3.  When a 'CALL' button is pressed, the elevator goes to that floor. 
+//       4.  REQUEST or CALL is acknowledged with a light.
 //       4.  Once a CALL or a REQUEST is accepted, others are ignored until the elevator gets
 //           to the desired floor.
 
@@ -28,45 +29,107 @@
 // These functions are mandatory.  They must be implement with the same name and arguments
 void controller_tick()
 {
+
+}
+void controller_init()
+{
 }
 
-int some_function()
+void DOOR_IS_OPEN()
 {
-	return 0;
+        printf("DOOR_IS_OPEN event\n");
+}
+
+void DOOR_IS_CLOSED()
+{
+        printf("DOOR_IS_CLOSED event\n");
+}
+
+void DOOR_IS_OBSTRUCTED()
+{
+        printf("DOOR_IS_OBSTRUCTED event\n");
+}
+
+void CAB_POSITION_FLOOR_2()
+{
+        printf("CAB_POSITION_FLOOR_2 event\n");
+}
+
+void CAB_POSITION_FLOOR_2_5()
+{
+        printf("CAB_POSITION_FLOOR_2_5 event\n");
+}
+
+void CAB_POSITION_FLOOR_3()
+{
+        printf("CAB_POSITION_FLOOR_3 event\n");
+}
+
+void CAB_POSITION_FLOOR_3_5()
+{
+        printf("CAB_POSITION_FLOOR_3_5 event\n");
+}
+
+void CAB_POSITION_FLOOR_4()
+{
+        printf("CAB_POSITION_FLOOR_4 event\n");
 }
 
 // The rest of these functions are not mandatory.  Students can implement the logic using 
 // any design, however it must be using the C language.
 //
+typedef enum {OFF,FLOOR2,FLOOR3,FLOOR4,GOINGTO2,GOINGTO3,GOINGTO4} elevatorStateEnum;
+static volatile elevatorStateEnum currentState;
+
 void off_state_entry()
 {
+        currentState = OFF;
 }
 
 void floor2_state_entry()
 {
+        currentState = FLOOR2;
 }
 
 void floor3_state_entry()
 {
+        currentState = FLOOR3;
 }
 
 void floor4_state_entry()
 {
+        currentState = FLOOR4;
 }
 
 void goingto2_state_entry()
 {
+        currentState = GOINGTO2;
 }
 
 void goingto3_state_entry()
 {
+        currentState = GOINGTO3;
 }
 
 void goingto4_state_entry()
 {
+        currentState = GOINGTO4;
 }
 
-typedef enum {OFF,FLOOR2,FLOOR3,FLOOR4,GOINGTO2,GOINGTO3,GOINGTO4} elevatorStateEnum;
-static elevatorStateEnum currentState,nextState;
+void (*fsm[8][GOINGTO4+1]) (void)={
+                         {0,0,0,0,0,0,0}, // OFF
+                         {0,0,0,0,0,0,0}, // FLOOR2
+                         {0,0,0,0,0,0,0}, // FLOOR3
+                         {0,0,0,0,0,0,0}, // FLOOR4
+                         {0,0,0,0,0,0,0}, // GOINGTO2
+                         {0,0,0,0,0,0,0}, // GOINGTO3
+                         {0,0,0,0,0,0,0}, // GOINGTO4 
+                       }; 
 
-
+void transition(int eventNumber)
+{
+        if (fsm[eventNumber][currentState])
+        {
+             fsm[eventNumber][currentState]();
+        }
+}
--- src/elevator.c
+++ src/elevator.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
 
 #include "elevator.h"
+#include "events_from_elevator.h"
+
 
 // these variables can be written to by another thread,
 // thus the volatile qualifier
-static volatile unsigned int elapsedTime;
-// seconds
-static volatile unsigned int power;
-// 0 is off 1 is on
-static volatile unsigned int cabPosition;
-// 20 to 40.  20 is the second floor, 30 is the third floor, etc.
-static volatile int cabDirection;
-// -1 is down, +1 is up.  and 0 is neither
+static volatile unsigned int elapsedTime; // seconds
+static volatile unsigned int power;       // 0 is off 1 is on
+static volatile unsigned int cabPosition; // 20 to 40.  20 is the second floor,
+                                          // 30 is the third floor, etc.
+static volatile int cabDirection;         // -1 is down, +1 is up.  and 0 is neither
+static volatile int doorPosition;         // 0 is open, 5 is fully closed
+static volatile int doorDirection;        // -1 is closing, +1 is opening, 0 is not moving
 
 void init_elevator()
 {
-    // this function is called to initialize the simulated elevator
-    elapsedTime = 0;
-    power = 0;
-    cabPosition = 20; // ground floor
-    cabDirection = 1; // not moving
+        // this function is called to initialize the simulated elevator
+        elapsedTime = 0;
+        power = 0;
+        cabPosition = 20; // ground floor
+        cabDirection = 1; // not moving
+        doorPosition = 5; // closed
+        doorDirection = 0;
 }
+
 void elevator_tick()
 {
-    printf("elevator tick called\n");
-    printf("cab position %d\n",cabPosition);
+    //printf("elevator tick called\n");
     if (power)
     {
+        //////////////////////////////////////////////
         // move the time forward
         elapsedTime++;
 
+        //////////////////////////////////////////////
         // move the cab, if so desired
         if (cabDirection==0)
         {
@@ -52,42 +57,98 @@ void elevator_tick()
              }
         }
 
+        //////////////////////////////////////////////
+        // deal with the door
+        if (doorDirection==0)
+        {
+        }
+        else if (doorDirection==1)
+        {
+           if (doorDirection<5)
+           {
+               doorPosition++;
+           }
+        }
+        else if (doorDirection==-1)
+        {
+           if (doorPosition>1)
+           {
+              doorPosition--;
+           }
+        }
+
+
+
+
+        //////////////////////////////////////////////
         // generate any events for the elevator controller
-        //
-        //
-        //
-        //
-    }
+        // these events are only generated once, each time they 
+        // occur
+        if (doorDirection)
+        {
+                if (doorPosition==1)
+                {
+                        DOOR_IS_OPEN();
+                }
+                else if (doorPosition==5)
+                {
+                        DOOR_IS_CLOSED();
+                }
+        }
+        // figure this out.... DOOR_IS_OBSTRUCTED();
+
+        if (cabDirection)
+        {
+                if (cabPosition == 20)
+                {
+                        CAB_POSITION_FLOOR_2();
+                }
+                else if (cabPosition == 25)
+                {
+                        CAB_POSITION_FLOOR_2_5();
+                }
+                else if (cabPosition == 30)
+                {
+                        CAB_POSITION_FLOOR_3();
+                }
+                else if (cabPosition == 35)
+                {
+                        CAB_POSITION_FLOOR_3_5();
+                }
+                else if (cabPosition == 40)
+                {
+                        CAB_POSITION_FLOOR_4();
+                }       
+        } // cab is moving
+
+    } // power is on
 }
 
 unsigned int timeInSeconds()
 {
-    return elapsedTime;
+        return elapsedTime;
 }
 
 void power_on()
 {
-    printf("power on called\n");
-    if (!power)
-    {
-       init_elevator();
-       power = 1;
-    }
+        if (!power)
+        {
+                init_elevator();
+                power = 1;
+        }
 }
 
 void power_off()
 {
-    printf("power off called\n");
-    power = 0;
+        power = 0;
 }
 
 int power_status()
 {
-        printf("power status called %d\n",power);
         return power;
 }
 
 int cab_position()
 {
-     return cabPosition;
+        return cabPosition;
 }
